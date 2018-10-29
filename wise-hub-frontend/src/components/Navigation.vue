87414b7b2e9fae830bf8032ac440e8d5a79e8c79
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-29 14:22:27 +0100

    feat: style header on mobile

--- wise-hub-frontend/src/components/Navigation.vue
+++ wise-hub-frontend/src/components/Navigation.vue
@@ -1,24 +1,40 @@
 <!-- src/components/Navigation.vue -->
 <template>
     <b-navbar id="navigation-component" toggleable="md" type="light">
-        <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+        <b-navbar-toggle class="main-nav-toggler" :target="uuid + '-main_nav_collapse'"></b-navbar-toggle>
 
-        <b-collapse is-nav id="nav_collapse">
+        <b-collapse is-nav :id="uuid + '-main_nav_collapse'" class="main-nav-collapser">
 
             <b-navbar-nav>
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/"><font-awesome-icon :icon="trendingIcon" /> Read</router-link>
+                <li class="nav-item" v-if="username.length > 0">
+                    <router-link class="nav-link" :to="'/@' + username">
+                        <em><span class="d-inline-block">@</span>{{ username }}</em>
+                    </router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="nav-link" to="/vote"><font-awesome-icon :icon="voteIcon" /> Vote</router-link>
+                    <router-link class="nav-link" to="/">
+                        <font-awesome-icon :icon="trendingIcon" /> Read
+                    </router-link>
                 </li>
                 <li class="nav-item">
-                    <router-link class="nav-link" to="/delegate"><font-awesome-icon :icon="delegateIcon" /> Delegate</router-link>
+                    <router-link class="nav-link" to="/vote">
+                        <font-awesome-icon :icon="voteIcon" /> Vote
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/delegate">
+                        <font-awesome-icon :icon="delegateIcon" /> Delegate
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <a :href="manualUrl" class="nav-link" target="_blank" rel="nofollow noopener">
+                        <font-awesome-icon :icon="manualIcon" /> Manual <!--<sup><font-awesome-icon :icon="externalLinkIcon" /></sup>-->
+                    </a>
                 </li>
             </b-navbar-nav>
-
-            <profile-button />
         </b-collapse>
+
+        <account-button />
     </b-navbar>
 </template>
 
@@ -26,24 +42,32 @@
 import Vue from "vue";
 import { icons } from "../icons";
 import { s } from "../store/store";
+import { uniqueId } from "../util/util";
 
-import ProfileButton from "./controls/ProfileButton.vue";
+import AccountButton from "./controls/account/AccountButton.vue";
 
 export default Vue.extend({
     props: [],
     data() {
         return {
+            uuid: uniqueId(),
+            manualUrl: /*§ §*/ "https://wise.vote/introduction" /*§ ' "' + data.config.manual.url.production + '" ' §.*/
         };
     },
     methods: {
     },
     computed: {
+        username(): string {
+            return s(this.$store).state.user.username || "";
+        },
         delegateIcon() { return icons.delegator; },
         voteIcon() { return icons.voter; },
         trendingIcon() { return icons.read; },
+        manualIcon() { return icons.manual; },
+        externalLinkIcon() { return icons.externalLink; },
     },
     components: {
-        ProfileButton,
+        AccountButton,
     },
 });
 </script>
@@ -63,5 +87,20 @@ export default Vue.extend({
 #navigation-component .nav-item .nav-link {
     padding-left: 1rem;
     padding-right: 1rem;
+    text-align: center; /* center icons on sm screen */
+}
+
+#navigation-component .show .nav-item .nav-link,
+#navigation-component .collapsing .nav-item .nav-link {
+    text-align: left;
+    padding: 1rem;
+}
+
+#navigation-component .main-nav-toggler {
+}
+
+#navigation-component .main-nav-collapser.show,
+#navigation-component .main-nav-collapser.collapsing {
+    margin-bottom: 1rem;
 }
 </style>
