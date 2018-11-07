d33575b33faa2b57f0bcab4b9c16fb4ec1646c7c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-11-07 01:12:21 +0100

    fix: allow no tls verify in secrets install

--- install-secrets.sh
+++ install-secrets.sh
@@ -3,19 +3,22 @@ set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # parent dir of scripts dir
 cd "${DIR}"
 
-
 JQ="jq"
-VAULT_ADDR="http://127.0.0.1:8200"
+VAULT_ADDR="https://127.0.0.1:8200"
 # USERPASS_USERNAME=""
 # USERPASS_PASS=""
 SECRETS_PATH="/run/wise/secrets"
 
 mkdir -p ${SECRETS_PATH}
 
-TOKEN=$(curl -s \
+echo "Vault status:"
+curl -k https://127.0.0.1:8200/v1/sys/health
+
+echo "Getting token"
+TOKEN=$(curl -k -s \
     --request POST \
     --data "{\"password\":\"${USERPASS_PASS}\"}" \
-    http://127.0.0.1:8200/v1/auth/userpass/login/${USERPASS_USERNAME} | ${JQ} --raw-output .auth.client_token)
+    https://127.0.0.1:8200/v1/auth/userpass/login/${USERPASS_USERNAME} | ${JQ} --raw-output .auth.client_token)
 
 
 #§ 'API_ROLE_NAME="' + data.config.hub.docker.services.api.appRole.role + '"'
@@ -34,48 +37,52 @@ SECRETNAME_PUBLISHER_ROLE_SECRET="hub-publisher-approle-secret"
 
 
 
-
-API_ROLE_ID=$(curl -s \
+echo "Getting api_role_id"
+API_ROLE_ID=$(curl -k -s \
     --header "X-Vault-Token: ${TOKEN}" \
-    http://127.0.0.1:8200/v1/auth/approle/role/$API_ROLE_NAME/role-id | ${JQ} --raw-output .data.role_id)
+    https://127.0.0.1:8200/v1/auth/approle/role/$API_ROLE_NAME/role-id | ${JQ} --raw-output .data.role_id)
 
 if [ -e "${SECRETS_PATH}/${SECRETNAME_API_ROLE_SECRET}" ]; then 
+    echo "Destroying old secret"
     OLD_SECRET_ID=$(<${SECRETS_PATH}/${SECRETNAME_API_ROLE_SECRET})
-    curl --fail \
+    curl -k --fail \
         --header "X-Vault-Token: ${TOKEN}" \
         --data "{\"secret_id\":\"aa${OLD_SECRET_ID}j\"}" \
-        http://127.0.0.1:8200/v1/auth/approle/role/$API_ROLE_NAME/secret-id/destroy
-    echo "Destroyed token ${OLD_SECRET_ID}"
+        https://127.0.0.1:8200/v1/auth/approle/role/$API_ROLE_NAME/secret-id/destroy
+    echo "Destroyed old secret"
 fi
 
-API_SECRET_ID=$(curl -s \
+echo "Getting API_SECRET_ID"
+API_SECRET_ID=$(curl -k -s \
     --header "X-Vault-Token: ${TOKEN}" \
     --data "{\"metadata\":\"generator=install-secrets\"}" \
-    http://127.0.0.1:8200/v1/auth/approle/role/$API_ROLE_NAME/secret-id | ${JQ} --raw-output .data.secret_id)
+    https://127.0.0.1:8200/v1/auth/approle/role/$API_ROLE_NAME/secret-id | ${JQ} --raw-output .data.secret_id)
 
 printf "${API_ROLE_ID}" > "${SECRETS_PATH}/${SECRETNAME_API_ROLE_ID}"
 printf "${API_SECRET_ID}" > "${SECRETS_PATH}/${SECRETNAME_API_ROLE_SECRET}"
 
 
 
-
-PUBLISHER_ROLE_ID=$(curl -s \
+echo "Getting PUBLISHER_ROLE_ID"
+PUBLISHER_ROLE_ID=$(curl -k -s \
     --header "X-Vault-Token: ${TOKEN}" \
-    http://127.0.0.1:8200/v1/auth/approle/role/$PUBLISHER_ROLE_NAME/role-id | ${JQ} --raw-output .data.role_id)
+    https://127.0.0.1:8200/v1/auth/approle/role/$PUBLISHER_ROLE_NAME/role-id | ${JQ} --raw-output .data.role_id)
 
 if [ -e "${SECRETS_PATH}/${SECRETNAME_PUBLISHER_ROLE_SECRET}" ]; then 
     OLD_SECRET_ID=$(<${SECRETS_PATH}/${SECRETNAME_PUBLISHER_ROLE_SECRET})
-    curl --fail \
+    echo "Destroying old secret"
+    curl -k --fail \
         --header "X-Vault-Token: ${TOKEN}" \
         --data "{\"secret_id\":\"aa${OLD_SECRET_ID}j\"}" \
-        http://127.0.0.1:8200/v1/auth/approle/role/$PUBLISHER_ROLE_NAME/secret-id/destroy
-    echo "Destroyed token ${OLD_SECRET_ID}"
+        https://127.0.0.1:8200/v1/auth/approle/role/$PUBLISHER_ROLE_NAME/secret-id/destroy
+    echo "Destroyed old secret"
 fi
 
-PUBLISHER_SECRET_ID=$(curl -s \
+echo "Getting PUBLISHER_SECRET_ID"
+PUBLISHER_SECRET_ID=$(curl -k -s \
     --header "X-Vault-Token: ${TOKEN}" \
     --data "{\"metadata\":\"generator=install-secrets\"}" \
-    http://127.0.0.1:8200/v1/auth/approle/role/$PUBLISHER_ROLE_NAME/secret-id | ${JQ} --raw-output .data.secret_id)
+    https://127.0.0.1:8200/v1/auth/approle/role/$PUBLISHER_ROLE_NAME/secret-id | ${JQ} --raw-output .data.secret_id)
 
 printf "${PUBLISHER_ROLE_ID}" > "${SECRETS_PATH}/${SECRETNAME_PUBLISHER_ROLE_ID}"
 printf "${PUBLISHER_SECRET_ID}" > "${SECRETS_PATH}/${SECRETNAME_PUBLISHER_ROLE_SECRET}"
