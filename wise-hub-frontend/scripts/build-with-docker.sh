11b640bc7a94c5f7158e2a08dc81680d8b7aafe0
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-11-22 22:52:08 +0100

    ci(frontend): build frontend server-side

---
+++ wise-hub-frontend/scripts/build-with-docker.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e # fail on first error
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.." # go to parent dir
+cd "${DIR}"
+
+#§ 'IMAGE=node:"' + data.config.npm.node.version + '-slim"'
+IMAGE=node:10.12-slim
+
+docker run -it \
+  -w "/app" \
+  -v "${PWD}:/app" \
+  "${IMAGE}" sh -c "npm install && npm run build-production"
--- wise-hub-frontend/src/store/modules/auth/User.ts
+++ wise-hub-frontend/src/store/modules/auth/User.ts
@@ -1,2 +1,29 @@
-// this works since webpack outputs all compiled code into User
-export * from "../../../../../backend/src/common/model/User";
+
+import * as steem from "steem";
+
+export interface User {
+    account: string;
+    profile: undefined | {
+        name: string;
+        account: steem.AccountInfo;
+        scope: string [];
+        user_metadata: any;
+    };
+    scope: string [];
+    settings?: UserSettings;
+}
+
+export interface UserSettings {
+    daemonEnabled: boolean;
+}
+
+/**
+ * This is an TS 1.6+ TypeGuard as described here: https://www.typescriptlang.org/docs/handbook/advanced-types.html
+ */
+export function isUserSettings(o: object): o is UserSettings {
+    return (<UserSettings>o).daemonEnabled !== undefined;
+}
+
+export const defaultUserSettings = {
+    daemonEnabled: false
+};
