2420d20b725894da79fe06042f275a5cd2d180e2
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-12-01 10:23:04 +0100

    test(frontend): modify WindowContext to allow mocking

--- wise-hub-frontend/src/WindowContext.ts
+++ wise-hub-frontend/src/WindowContext.ts
@@ -1,10 +1,20 @@
-declare const _WINDOW_CONTEXT: any;
+// declare const _WINDOW_CONTEXT: any;
 
 export namespace WindowContext {
-    export const ENVIRONMENT_TYPE: string = _WINDOW_CONTEXT.ENVIRONMENT_TYPE;
-    export const WISE_SQL_ENDPOINT_URL: string = _WINDOW_CONTEXT.WISE_SQL_ENDPOINT_URL;
-    export const STEEMD_ENDPOINT_URL: string = _WINDOW_CONTEXT.STEEMD_ENDPOINT_URL;
-    export const STEEMCONNECT_REDIRECT_URI: string = _WINDOW_CONTEXT.STEEMCONNECT_REDIRECT_URI;
+    export const isBrowser = ( typeof window !== "undefined" );
+
+    export let ENVIRONMENT_TYPE: string = 
+        isBrowser ? (window as any)._WINDOW_CONTEXT.ENVIRONMENT_TYPE : "local-nonbrowser"; // window is null when testing
+    
+    export let WISE_SQL_ENDPOINT_URL: string =
+        isBrowser ? (window as any)._WINDOW_CONTEXT.WISE_SQL_ENDPOINT_URL : "https://sql.local";
+    
+    export let STEEMD_ENDPOINT_URL: string = 
+        isBrowser ? (window as any)._WINDOW_CONTEXT.STEEMD_ENDPOINT_URL : "https://steemd.local";
+    
+    export let STEEMCONNECT_REDIRECT_URI: string =
+        isBrowser ? (window as any)._WINDOW_CONTEXT.STEEMCONNECT_REDIRECT_URI : "https://sc_redirect.local";
+
 
     export function failIfMissing() {
         if (!ENVIRONMENT_TYPE) throw new Error("Window context variable ENVIRONMENT_TYPE is missing. Please contact Wise-team.");
