6d4b87b2eb4f16013f7b16aea9a9a4ef747e9d77
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-12-01 10:20:56 +0100

    feat(api): add accounts routes

---
+++ backend/src/api/routes/AccountsRoutes.ts
@@ -0,0 +1,37 @@
+import { Redis } from "ioredis";
+import * as express from "express";
+import { UsersManager } from "../../lib/UsersManager";
+import { asyncReq, d } from "../lib/util";
+import { AuthManager } from "../auth/AuthManager";
+import { User, isUserSettings } from "../../common/model/User";
+import { common } from "../../common/common";
+
+export class AccountsRoutes {
+    private redis: Redis;
+    private usersManager: UsersManager;
+
+    public constructor(redis: Redis, usersManager: UsersManager) {
+        this.redis = redis;
+        this.usersManager = usersManager;
+    }
+
+    public async init() {
+
+    }
+
+    public routes(app: express.Application) {
+        app.get(common.urls.api.accounts.base + "/:account/settings",
+            (req, res) => asyncReq(res, async () => {
+                const accountName = d(req.params.delegator);
+                const user: User | undefined = await this.usersManager.getUser(accountName);
+                if (!user) {
+                    res.status(404);
+                    res.send("Not found.");
+                }
+                else {
+                    res.send(JSON.stringify(user.settings));
+                }
+            })
+        );
+    }
+}
