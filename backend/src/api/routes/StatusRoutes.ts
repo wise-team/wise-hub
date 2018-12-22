8f21254c796e67f8e1ce276597f96d8c73f032ee
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-12-22 12:22:15 +0100

    feat: include verdict in api status route (allows monitoring with tools like uptime robot)

--- backend/src/api/routes/StatusRoutes.ts
+++ backend/src/api/routes/StatusRoutes.ts
@@ -1,4 +1,5 @@
 import { Redis } from "ioredis";
+import * as BluebirdPromise from "bluebird";
 import * as express from "express";
 import { UsersManager } from "../../lib/UsersManager";
 import { Vault } from "../../lib/vault/Vault";
@@ -20,40 +21,7 @@ export class StatusRoutes {
 
     public routes(app: express.Application) {
         app.get("/api/status", (req, res) => asyncReq("api/routes/StatusRoutes.ts route status", res, async () => {
-            const sTime = Date.now();
-            let publicSecret: any = undefined;
-            const vaultStatus: { initialized?: boolean; sealed?: boolean; error?: string; } = {};
-            try {
-                const vaultStatusResp = await this.vault.getStatus();
-                vaultStatus.initialized = vaultStatusResp.initialized;
-                vaultStatus.sealed = vaultStatusResp.sealed;
-
-                publicSecret = await this.vault.getSecret("/hub/public/status");
-            }
-            catch (error) { vaultStatus.error = error + ""; }
-
-            const daemon = await this.redis.hgetall(common.redis.daemonStatus.key);
-            const alive = {
-                publisher: await this.redis.exists(common.redis.publisherHartbeat) > 0,
-                daemon: await this.redis.exists(common.redis.daemonHartbeat) > 0,
-                realtime: await this.redis.exists(common.redis.realtimeHartbeat) > 0,
-            };
-
-            const publisher = {
-                queueLen: await this.redis.llen(common.redis.toPublishQueue),
-                processingLen: await this.redis.llen(common.redis.publishProcessingQueue)
-            };
-
-            const took = Date.now() - sTime;
-
-            const payload: StatusResponsePayload = {
-                vault: vaultStatus,
-                alive: alive,
-                publicSecret: publicSecret,
-                daemon: daemon,
-                publisher: publisher,
-                took: took
-            };
+            const payload: StatusResponsePayload = await this.getStatus();
 
             res.send(JSON.stringify(payload));
         }));
@@ -67,6 +35,78 @@ export class StatusRoutes {
                 processing: publishProcessingQueue
             }));
         }));
+    }
+
+    private async getStatus(): Promise<StatusResponsePayload> {
+        await BluebirdPromise.delay(100); // This is heavy request (~1500ms), so in order to prevent DDoS we include this small delay
+        const sTime = Date.now();
+        let publicSecret: any = undefined;
+        const vaultStatus: { initialized?: boolean; sealed?: boolean; error?: string; } = {};
+        try {
+            const vaultStatusResp = await this.vault.getStatus();
+            vaultStatus.initialized = vaultStatusResp.initialized;
+            vaultStatus.sealed = vaultStatusResp.sealed;
+
+            publicSecret = await this.vault.getSecret("/hub/public/status");
+        }
+        catch (error) { vaultStatus.error = error + ""; }
+
+        const daemon = await this.redis.hgetall(common.redis.daemonStatus.key);
+        const alive = {
+            publisher: await this.redis.exists(common.redis.publisherHartbeat) > 0,
+            daemon: await this.redis.exists(common.redis.daemonHartbeat) > 0,
+            realtime: await this.redis.exists(common.redis.realtimeHartbeat) > 0,
+        };
+
+        const publisher = {
+            queueLen: await this.redis.llen(common.redis.toPublishQueue),
+            processingLen: await this.redis.llen(common.redis.publishProcessingQueue)
+        };
+
+        const took = Date.now() - sTime;
+
+        const payload: StatusResponsePayload = {
+            vault: vaultStatus,
+            alive: alive,
+            publicSecret: publicSecret,
+            daemon: daemon,
+            publisher: publisher,
+            took: took,
+            verdict: { type: "VERDICT_UNHEALTHY", msg: "Verdict not yet processed" }
+        };
+        payload.verdict = this.getStatusVerdict(payload);
+        return payload;
+    }
+
+    private getStatusVerdict(s: StatusResponsePayload): { type: "VERDICT_UNHEALTHY" | "VERDICT_HEALTHY", msg: string } {
+        if (s.vault.error && s.vault.error.length > 0)
+            return { type: "VERDICT_UNHEALTHY", msg: "Vault error: " + s.vault.error };
+
+        if (!s.vault.initialized)
+            return { type: "VERDICT_UNHEALTHY", msg: "Vault not initialized" };
+
+        if (s.vault.sealed)
+            return { type: "VERDICT_UNHEALTHY", msg: "Vault is sealed" };
+
+        if (!s.alive.daemon)
+            return { type: "VERDICT_UNHEALTHY", msg: "Daemon is not alive" };
+
+        if (!s.alive.publisher)
+            return { type: "VERDICT_UNHEALTHY", msg: "Publisher is not alive" };
+
+        if (!s.alive.realtime)
+            return { type: "VERDICT_UNHEALTHY", msg: "Realtime is not alive" };
+
+        if (s.publisher.queueLen > 15)
+            return { type: "VERDICT_UNHEALTHY", msg: "Queue length > 15" };
+
+        if (s.publisher.processingLen > 15)
+            return { type: "VERDICT_UNHEALTHY", msg: "Processing queue length > 15" };
+
+        if (s.took > 3000)
+            return { type: "VERDICT_UNHEALTHY", msg: "Status generation took > 3s" };
+
+        return { type: "VERDICT_HEALTHY", msg: "" };
     }
 }
 
@@ -88,4 +128,8 @@ export interface StatusResponsePayload {
     daemon: any;
     publicSecret: any;
     took: number;
+    verdict: {
+        type: "VERDICT_HEALTHY" | "VERDICT_UNHEALTHY";
+        msg: string;
+    };
 }
