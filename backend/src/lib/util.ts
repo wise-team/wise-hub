036ecc7709b7ac524fce45fd13b4bb041caecbc6
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-10 14:11:59 +0100

    feat: nest ow checks using ow_catch

--- backend/src/lib/util.ts
+++ backend/src/lib/util.ts
@@ -1,13 +1,22 @@
-export function d <T> (input: T | undefined): T {
+export function d<T>(input: T | undefined): T {
     if (typeof input !== "undefined") return input;
     else throw new Error("Input value is undefined (d() fn)");
 }
 
-export function i <T, R> (i: T | undefined, fn: ($: T) => R): R {
+export function i<T, R>(i: T | undefined, fn: ($: T) => R): R {
     return d(fn(d(i)));
 }
 
-export function assertString <T> (input: T | undefined): T {
+export function assertString<T>(input: T | undefined): T {
     if (typeof input !== "string") throw new Error("Input value is undefined (d() fn)");
     return input;
+}
+
+export function ow_catch(fn: () => void): boolean | string {
+    try {
+        fn();
+        return true;
+    } catch (error) {
+        return error + "";
+    }
 }
