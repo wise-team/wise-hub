0df7cfe273d5f46c46248a50567b21df22a9ab61
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-15 18:43:09 +0100

    fix(log): allow string-only error logging

--- backend/src/lib/Log.ts
+++ backend/src/lib/Log.ts
@@ -9,26 +9,24 @@ export class Log extends AbstractLog {
     }
 
     public initialize() {
-        super.init([
-            process.env.WISE_HUB_BACKEND_LOG_LEVEL,
-            process.env.WISE_LOG_LEVEL,
-            "info"
-        ]);
+        super.init([process.env.WISE_HUB_BACKEND_LOG_LEVEL, process.env.WISE_LOG_LEVEL, "info"]);
     }
 
     public init() {
         throw new Error("Instead of #init() please call #initialize(debug, verbose) which indirectly overrides init");
     }
 
-    public logError(debugInfo: string, error: Error, attachement: any = undefined): void {
+    public logError(debugInfo: string, error?: Error, attachement: any = undefined): void {
         const stack = new Error().stack;
-        this.error(JSON.stringify({
-            logMsgType: "error",
-            logger: { name: this.getName(), stack: stack },
-            debug: { info: debugInfo },
-            attachement: attachement,
-            ...serializeError(error),
-        }));
+        this.error(
+            JSON.stringify({
+                logMsgType: "error",
+                logger: { name: this.getName(), stack: stack },
+                debug: { info: debugInfo },
+                attachement: attachement,
+                ...(error ? serializeError(error) : {}),
+            })
+        );
     }
 
     public static log(): Log {
