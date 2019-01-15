821e7d65fba6cf2b64d7f99ee9cc0546f96b6a89
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-15 18:48:24 +0100

    feat(publisher): move all statics to StaticConfig

--- backend/src/publisher/StaticConfig.ts
+++ backend/src/publisher/StaticConfig.ts
@@ -1,4 +1,10 @@
-
 export class StaticConfig {
-    public static PUBLISH_THROTTLING_MS: number = 1000 * 5; // 3 seconds
+    public static REQUIRED_VAULT_POLICIES: string[] = /*§ §*/ [
+        "wise-hub-publisher",
+    ] /*§ JSON.stringify(data.config.hub.docker.services.publisher.appRole.policies(data.config)) §.*/;
+    public static PUBLISH_THROTTLING_MS: number = 1000 * 5; // 5 seconds
+    public static BROADCAST_SCOPE: string[] = ["custom_json", "vote"];
+    public static JOB_BLOCKINGWAIT_SECONDS = 20;
+    public static HEARTBEAT_TTL_SECONDS = Math.floor(StaticConfig.JOB_BLOCKINGWAIT_SECONDS * 2.5);
+    public static RETRIES_DELAYS_SECONDS: number[] = [3, 10, 30, 120, 300]; // length of this array signals the maximal number of retries
 }
