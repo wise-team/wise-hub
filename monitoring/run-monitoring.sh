83e112fa03214ba84b553a42ebae6f2b40ecdd66
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2019-01-28 15:38:07 +0100

    feat(monitoring): use dockerized-mocha-slack-service-monitoring for monitoring

--- monitoring/run-monitoring.sh
+++ monitoring/run-monitoring.sh
@@ -2,21 +2,48 @@
 set -e # fail on first error
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
+NAME="wise-hub monitoring(run-monitoring.sh)"
+
 
 if [ -z "${SLACK_WEBHOOK_URL}" ]; then
-    echo "wise-hub monitoring: SLACK_WEBHOOK_URL env is not set"
+    echo "$NAME: SLACK_WEBHOOK_URL env is not set"
     exit 1
 fi
 
 if [ -z "${WISE_ENVIRONMENT_TYPE}" ]; then
-    echo "wise-hub monitoring: WISE_ENVIRONMENT_TYPE env is not set"
+    echo "$NAME: WISE_ENVIRONMENT_TYPE env is not set"
+    exit 1
+fi
+
+if [ -z "${SLACK_MENTIONS}" ]; then
+    echo "$NAME: SLACK_MENTIONS env is not set"
     exit 1
 fi
 
 if [ -z "${WISE_HUB_URL}" ]; then
-    echo "wise-hub monitoring: WISE_HUB_URL env is not set"
+    echo "$NAME: WISE_HUB_URL env is not set"
     exit 1
 fi
+
+if [ -z "${FAILURE_NOTIFICATION_INTERVAL_S}" ]; then
+    echo "$NAME: FAILURE_NOTIFICATION_INTERVAL_S env is not set"
+    exit 1
+fi
+
+DATA_VOLUME="hub-monitoring-datavolume"
+
+docker run --rm \
+    --name "hub-monitoring" \
+    -e "SLACK_WEBHOOK_URL=${SLACK_WEBHOOK_URL}" \
+    -e "ENVIRONMENT_TYPE=localtest" \
+    -e "SLACK_MENTIONS=${SLACK_MENTIONS}" \
+    -e "PROJECT_NAME=wise-hub" \
+    -e "FAILURE_NOTIFICATION_INTERVAL_S=${FAILURE_NOTIFICATION_INTERVAL_S}" \
+    -v "$PWD:/spec" \
+    -v "${DATA_VOLUME}:/data" \
+    -e "WISE_HUB_URL=${WISE_HUB_URL}" \
+    wiseteam/dockerized-mocha-slack-service-monitoring
+
 
 
 DATA_VOLUME="hub_monitoring_data"
